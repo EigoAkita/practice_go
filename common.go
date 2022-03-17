936fef7897d42bb67c76c03766e0502c49ca0775
NLINKS\3202 <e-akita@n-links.co.jp>
2022-03-17 15:22:25 +0900

    下記、4項目の練習内容追加 ①変数 ②スライス（配列） ③メソッド ④クロージャ―

--- common.go
+++ common.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strconv"
-	// "strings"
 )
 
 const Pi = 3.14
@@ -26,9 +25,13 @@ func constFunc() {
 		x++
 		fmt.Println("\n", x)
 	*/
-	fmt.Println("bool値返還")
-	practicBool()
-	typeConversion()
+	// fmt.Println("bool値返還")
+	// practicBool()
+	// typeConversion()
+	// praciceArray()
+	// practicMap()
+	practiceByte()
+
 	///文字列
 
 	///"Hello World"の3番目を取得
@@ -55,23 +58,154 @@ func typeConversion() {
 	xx := float64(x)
 
 	/*
-	%T = 変数の型
-	%v = 変数の値
-	%f = 変数の値をfloat型に変換
+		%T = 変数の型
+		%v = 変数の値
+		%f = 変数の値をfloat型に変換
 	*/
 	fmt.Printf("%T %v %f\n", xx, xx, xx)
 
 	var s string = "14"
 
 	/*
-	string型をint型に変換
-	Atoiの引数はstring
-	Atoiの返り値はint,errorの為、変数を定義する時は i , _ := となる
+		string型をint型に変換
+		Atoiの引数はstring
+		Atoiの返り値はint,error
+
+		変数定義時
+		※error
+
+		i, err := strconv.Atoi(s)
+		(err未使用の場合に限る)
+
+		※!error
+
+		i, _ := strconv.Atoi(s)
+
 	*/
-	
+
 	i, err := strconv.Atoi(s)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("ERROR")
 	}
 	fmt.Printf("%T %v", i, i)
+
+	// h := "Hello World"
+	// fmt.Printf(string(h[0]))
+}
+
+func praciceArray() {
+	/*
+		var a [2]int
+		a[0] = 200
+		a[1] = 300
+		fmt.Println(a)
+
+		var b [2]int = [2]int{100, 200}
+		fmt.Println(b)
+
+		var b []int = []int {100,200}
+		b = append(b,300)
+		fmt.Println(b)
+	*/
+
+	/*
+					n := []int{1, 2, 3, 4, 5, 6}
+					fmt.Println(n)
+
+					fmt.Println(n[2])
+					fmt.Println(n[2:4])
+					fmt.Println(n[:2])
+
+					var board = [][]int{
+						{1, 2, 3},
+						{4, 5, 6},
+						{7, 8, 9},
+					}
+
+					fmt.Println(board)
+					n = append(n, 100,200,300,400,500,600)
+					fmt.Println(n)
+
+
+				///int配列　長さ3　キャパシティ5　初期化
+				n := make([]int, 3, 5)
+				fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+				n = append(n, 0, 0)
+				fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+
+				n = append(n, 1, 2, 3, 4, 5, 6)
+				fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+
+
+			a := make([]int, 3)
+			fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+
+			///0のスライス　メモリ確保有
+			b := make([]int, 0)
+			fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+			///初期値nil 　 メモリ確保無
+			var c []int
+			fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+
+
+		// c := make([]int, 5)
+		c := make([]int, 0, 5)
+		fmt.Println(c)
+		for i := 0; i < 5; i++ {
+			c = append(c, i)
+			fmt.Println(c)
+		}
+		fmt.Println(c)
+	*/
+
+}
+
+func practicMap() {
+
+	m := map[string]int{"カフェオレ": 100, "バナナ": 100}
+	fmt.Println(m)
+	fmt.Println(m["カフェオレ"])
+
+	m["カフェオレ"] = 300
+	fmt.Println(m)
+	m["あんぱん"] = 150
+	fmt.Println(m)
+
+	///map内key無　出力0
+	fmt.Println(m["nil"])
+
+	///スライス内のkey存在確認　有 = true 無 = false
+	v, ok := m["カフェオレ"]
+	fmt.Println(v, ok)
+	_, ok2 := m["nothing"]
+	if !ok2 {
+		fmt.Println(ok2)
+	}
+
+	///メモリ上 空map作成
+	m2 := make(map[string]int)
+	m2["pc"] = 200
+	fmt.Println(m2)
+
+	///宣言しているが、メモリ上に代入するmapがnilの為ERRORになる
+	//varの時は初期値はnil
+	/*
+		var m3 map[string]int
+		m3["pc"] = 200
+		fmt.Println(m3)
+	*/
+
+}
+
+///ネットワーク系
+///ファイルの読込などで使用
+func practiceByte() {
+	b := []byte{72, 73}
+	fmt.Println(b)
+	fmt.Println(string(b))
+
+	c := []byte("HI")
+	fmt.Println(c)
+	fmt.Println(string(c))
 }
--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt"
 	// "os/user"
 	// "time"
 )
 
 ///変数宣言　=> var
 var (
-	n int    = 100
-	s string = "test"
+	// n int    = 100
+	// s string = "test"
 	// var t bool = true
 	// var f bool = false
-	t, f bool = true, false
+	// t, f bool = true, false
 )
 
 func main() {
-	fmt.Print(n, s, t, f)
+	// fmt.Print(n, s, t, f)
 	/// := ショート変数 => 関数内限定定義
-	xn := "\n"
-	xi := 1
-	xf64 := 1.2
-	xs := "2回目テスト"
-	xt := true
-	xf := false
-	fmt.Println(xn, xi, xs, xt, xf)
-	fmt.Printf("\n%T", xf64)
-	constFunc()
+	// xn := "\n"
+	// xi := 1
+	// xf64 := 1.2
+	// xs := "2回目テスト"
+	// xt := true
+	// xf := false
+	// fmt.Println(xn, xi, xs, xt, xf)
+	// fmt.Printf("\n%T", xf64)
 	// fmt.Println(user.Current())
 	// fmt.Print(itemCount())
+	// constFunc()
+	commonFunction()
 }
 
 /*
